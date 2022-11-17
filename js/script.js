4fa7f835c921c4061c6f66ac13970c630caa2412
Aniket Agrawal <agra.aniket@gmail.com>
2022-11-17 20:29:52 +0100

    Add preliminary ideas for password match validation

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// get match text element
+// set value to not match and color red at the beginning
+// if any of the values gets updated 
+// check for equality or emptiness
+// update text/color based on result of check
