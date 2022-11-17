7420902f3113a30e2b5f2b18a9d27cdb5a8e7ed0
Aniket Agrawal <agra.aniket@gmail.com>
2022-11-17 20:52:06 +0100

    Allow updating text after checking entries
    A function will check the values of currently updated and the other field
    entries. If any of these is empty, or if they are not equal, a non-match text
    is kept. Otherwise, a match text is kept. Event listeners check for
    changes to values of password or confirm password fields. If there
    is a change, the update text function is called to check for a match

--- js/script.js
+++ js/script.js
@@ -6,3 +6,24 @@ matchText.style.color = "red";
 // if any of the values gets updated 
 // check for equality or emptiness
 // update text/color based on result of check
+function updateText(current, other) {
+    if (current.value !== "" && other.value !=="" &&
+        current.value === other.value) {
+        matchText.textContent = "Passwords match";
+        matchText.style.color = "green";
+    }
+    else {
+        matchText.textContent = "* Passwords do not match";
+        matchText.style.color = "red";    
+    }
+}
+
+let passwdField = document.querySelector("#passwd");
+let confirmPwdField = document.querySelector("#confirm-pwd");
+passwdField.addEventListener("input", function (e) {
+    updateText(this, confirmPwdField);
+});
+
+confirmPwdField.addEventListener("input", function (e) {
+    updateText(this, passwdField);
+});
