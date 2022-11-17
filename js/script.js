fa1f7360957be343f578d22a7d43c51cda31a331
Aniket Agrawal <agra.aniket@gmail.com>
2022-11-17 20:31:50 +0100

    Display initial warning

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 // get match text element
+let matchText = document.querySelector(".match-text");
 // set value to not match and color red at the beginning
+matchText.textContent = "* Passwords do not match";
+matchText.style.color = "red";
 // if any of the values gets updated 
 // check for equality or emptiness
 // update text/color based on result of check
